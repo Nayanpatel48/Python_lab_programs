1effd3d95111f2c85e6169748ffc3a178b3a55a4
nayan <www.xil187@gmail.com>
2024-08-01 12:40:44 +0530

    my 4rd commit in this repo

---
+++ string/indexingAndSlicing.py
@@ -0,0 +1,13 @@
+str = "Nayan"
+#indexing example
+print(str[0])
+print(str[1])
+print(str[2])
+print(str[3])
+
+#accessing string from the last index
+print("Last character in string = "+str[-1])
+
+#slicing
+print(str[: 3])
+print(str[1 : 4])
---
+++ string/stringExp1.py
@@ -0,0 +1,10 @@
+#.capitalize() operation on string
+str = input("Enter String:")
+str=str.capitalize()
+print("after performing .capitalize() on string="+str)
+
+#.casefold()
+str=str.casefold()
+print("after performing .casefold() on string="+str)
+
+#multiple operations are available
